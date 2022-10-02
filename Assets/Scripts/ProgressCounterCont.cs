ae9388870144e3baceba5ba4dc157f2074a28b1e
Maciej Walczak <Maciekarp@users.noreply.github.com>
2022-10-02 17:13:06 -0400

    Added a few words in segment font

---
+++ Assets/Scripts/ProgressCounterCont.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressCounterCont : MonoBehaviour
+{
+    [SerializeField] private NumberController progressCont, totalCont;
+
+    [SerializeField] private float numberStep = -1.2f;
+
+    public int completed;
+    public int total;
+
+    private Vector3 initialLocation;
+
+    void Start() {
+        initialLocation = totalCont.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        totalCont.transform.localPosition = initialLocation + new Vector3(numberStep *((int) Mathf.Log10(total)), 0f,0f);
+        totalCont.SetNumber((float) total);
+        progressCont.SetNumber((float) completed);
+
+    }
+}
